9708621a1003d948c9cb6799770f084e797cccda
Dev <devharwani.work@gmail.com>
2025-07-14 15:45:34 +0000

    solve symbol error

--- server/index.ts
+++ server/index.ts
@@ -11,10 +11,12 @@ app.post("/fetchTokens", async (req, res) => {
     const { walletAddress } = req.body;
     try {
         const tokens = await fetchTokenAccounts(walletAddress);
+        console.log(tokens);
         return res.json({
             response: tokens
         }).status(200);
     } catch (error) {
+        console.log(error);
         return res.json({
             error: "Error fetching token accounts: " + error
         }).status(400);
@@ -57,7 +59,8 @@ async function fetchTokenAccounts(walletAddress: string) {
 
 async function getTokenInfo(mintAddress: string) {
     try {
-        const response = await axios.post(`https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY},`, {
+        
+        const response = await axios.post(`https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY}`, {
             "jsonrpc": "2.0",
             "id": 1,
             "method": "getAsset",
@@ -68,10 +71,12 @@ async function getTokenInfo(mintAddress: string) {
         )
         return {
             name: response.data.result.content.metadata.name,
-            symbol: response.data.result.content.token_info.symbol,
+            symbol: response.data.result.token_info.symbol,
             logoURI: response.data.result.content.links.image
         }
     } catch (error) {
         throw new Error("Error in getTokenInfo: " + error);
     }
 }
+
+app.listen(3001);
